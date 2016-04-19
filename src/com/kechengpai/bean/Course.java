a904b2de2ef8774661a48a75c00c0ef56b6cf1f8
ghit700 <332668280@qq.com>
2016-04-19 18:05:40 +0800

    数据库

---
+++ src/com/kechengpai/bean/Course.java
@@ -0,0 +1,20 @@
+package com.kechengpai.bean;
+
+public class Course {
+	private int c_id;
+	private String name;
+	private String teacher;
+	private String code;
+	private int numbers;
+	public Course(int c_id, String name, String teacher, String code,
+			int numbers) {
+		super();
+		this.c_id = c_id;
+		this.name = name;
+		this.teacher = teacher;
+		this.code = code;
+		this.numbers = numbers;
+	}
+	
+	
+}
--- src/com/kechengpai/model/CourseModel.java
+++ src/com/kechengpai/model/CourseModel.java
@@ -1,15 +1,52 @@
 package com.kechengpai.model;
 
+import com.alibaba.fastjson.JSON;
+import com.kechengpai.bean.Course;
+import com.kechengpai.bean.User;
 import com.kechengpai.jdbc.jdbc;
+import com.mysql.jdbc.PreparedStatement;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 
 public class CourseModel {
 
-	Connection con = null;
+	static Connection con = null;
 
 	public CourseModel() {
 		super();
 		this.con = jdbc.getConn();
 	}
 
+	public static String queryCourseList(String account, int type) {
+		int i = -1;
+		String sql = null;
+		if (type == 1) {
+			sql = "select c_id,name,teacher,code,numbers from course where account=?";
+		} else {
+			sql = "select c_id,name,teacher,code,numbers from course as a,student_course as b"
+					+ " where account=? and a.c_id=b.c_id";
+		}
+
+		PreparedStatement pstmt;
+		try {
+			pstmt = (PreparedStatement) con.prepareStatement(sql);
+			pstmt.setString(1, account);
+			ResultSet rs = pstmt.executeQuery();
+			List<Course> courses = new ArrayList<Course>();
+			while (rs.next()) {
+				Course course = new Course(rs.getInt(1), rs.getString(2),
+						rs.getString(3), rs.getString(4), rs.getInt(5));
+				courses.add(course);
+			}
+			return JSON.toJSONString(courses);
+
+		} catch (Exception e) {
+			// TODO: handle exception
+		}
+
+		return "-1";
+	}
 }
